ba353fec64e973c50c0318e4d06fa47e82e9d505
shiakanthula <shivakanthula@gmail.com>
2025-02-18 15:52:22 +0530

    init commit -m my first commit

--- src/test/java/Tests_practise/TestNG/Assertions.java
+++ src/test/java/Tests_practise/TestNG/Assertions.java
@@ -13,27 +13,29 @@ public class Assertions {
         public static void softAssertion(){
 
             String s1 ="Shiva";
-            String s2 = "shiva";
-
+            String s2 ="shiava";
             SoftAssert softAssert = new SoftAssert();
             softAssert.assertEquals(s1,s2);
-
+// To track multiple validation failures in a single test case (using soft assertions).
             String s3 ="Kanthula" ;
-            String s4 = "kanthula";
+            String s4 = "kanthuula";
             softAssert.assertEquals(s4,s3);
             System.out.println("Soft Assert executes even the test fails");
+            softAssert.assertAll(); // it collects all assertion failures during test execution are reported
 
-            softAssert.assertAll();
         }
+
+
+
         @Test()
         public static void hardAssert(){
 
             WebDriverManager.chromedriver().setup();
-
             WebDriver driver = new ChromeDriver();
 
             driver.get("https://github.com/shivakanthula/Automation");
             String actual = driver.getTitle();
+// To avoid unnecessary code execution after a critical failure (using hard assertions).
             String expected ="shvakanthula/Automation · GitHub";
             Assert.assertEquals(actual,expected);
             System.out.println("assert should fail");
